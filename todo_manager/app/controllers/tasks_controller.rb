8c211620cd4f4d7297420be6f2d637c18318d8f7
Hong Lu <hlmontgomory@gmail.com>
2015-01-15 14:05:14 +0800

    Added Edit page

--- todo_manager/app/controllers/tasks_controller.rb
+++ todo_manager/app/controllers/tasks_controller.rb
@@ -11,6 +11,10 @@ class TasksController < ApplicationController
   	@task = Task.new
   end
 
+	def edit
+	  @task = Task.find(params[:id])
+	end
+
 	def create
 	  @task = Task.new(task_params)
 	 
@@ -20,7 +24,17 @@ class TasksController < ApplicationController
 	    render 'new'
 	   end
 	end
+
+	def update
+	  @task = Task.find(params[:id])
 	 
+	  if @task.update(task_params)
+	    redirect_to @task
+	  else
+	    render 'edit'
+	  end
+	end
+
 	private
 	  def task_params
 	    params.require(:task).permit(:title, :text)
