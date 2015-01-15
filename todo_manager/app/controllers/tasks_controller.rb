fffee51d048f8b5c8e820244f4ed755bd45ebb4f
Hong Lu <hlmontgomory@gmail.com>
2015-01-15 13:07:57 +0800

    Added create method

--- todo_manager/app/controllers/tasks_controller.rb
+++ todo_manager/app/controllers/tasks_controller.rb
@@ -1,4 +1,17 @@
 class TasksController < ApplicationController
   def new
   end
+
+	def create
+	  @task = Task.new(task_params)
+	 
+	  @task.save
+	  redirect_to @task
+	end
+	 
+	private
+	  def task_params
+	    params.require(:task).permit(:title, :text)
+	  end
+
 end
