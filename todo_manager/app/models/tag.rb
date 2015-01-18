463fcf8077619365c4144d1f4a06e2b1958b2959
Hong Lu <hlmontgomory@gmail.com>
2015-01-18 20:14:17 +0800

    Added tag name validation Fixed #2

--- todo_manager/app/models/tag.rb
+++ todo_manager/app/models/tag.rb
@@ -1,4 +1,7 @@
 class Tag < ActiveRecord::Base
+  validates :name, format: { with: /\A[a-zA-Z]+\z/,
+  message: "only allows letters" }
+
   has_many :taggings
   has_many :tasks, through: :taggings
 end
