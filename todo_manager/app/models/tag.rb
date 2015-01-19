3d20756d1b4567c2dc0fe1c03f80bf34de5c1c3f
Hong Lu <hlmontgomory@gmail.com>
2015-01-19 08:21:25 +0800

    Modified regExp of new tag validation

--- todo_manager/app/models/tag.rb
+++ todo_manager/app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  validates :name, format: { with: /[a-zA-Z0-9]/,
+  validates :name, format: { with: /\A[a-z0-9_-]+\Z/,
   message: "only allows numbers and letters" }
 
   has_many :taggings
