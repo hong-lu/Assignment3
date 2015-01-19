c2ab11cb76da995a1ef370b562a44850da2bf289
Hong Lu <hlmontgomory@gmail.com>
2015-01-19 09:08:26 +0800

    Display relevant tasks on manage tag page

--- todo_manager/app/models/tag.rb
+++ todo_manager/app/models/tag.rb
@@ -1,7 +1,16 @@
 class Tag < ActiveRecord::Base
-  validates :name, format: { with: /\A[a-z0-9_-]+\Z/,
+  validates :name, format: { with: /\A[a-zA-Z0-9_-]+\z/,
   message: "only allows numbers and letters" }
 
   has_many :taggings
   has_many :tasks, through: :taggings
+
+  def self.delete_empty_tags
+  	Tag.all.each do |tag|
+  		if (tag.tasks.count == 0)
+  			tag.destroy
+  		end
+  	end
+  end
+
 end
